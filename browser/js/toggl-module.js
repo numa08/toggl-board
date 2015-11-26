360ac2df75f96cc04d34be33747854d76d9c1984
numa08 <n511287@gmail.com>
2015-11-26 09:35:12 +0900

    Input API_TOKEN tand show user list

--- browser/js/toggl-module.js
+++ browser/js/toggl-module.js
@@ -4,12 +4,14 @@ var TogglClient = require('toggl-api');
 var togglBoard = angular.module('TogglBoard', []);
 togglBoard.controller('TogglController', ['$scope', function($scope) {
 	
+	$scope.userDataCollection = [];
+	
 	$scope.addUser = function() {
 		 new TogglClient({apiToken: $scope.apiToken})
 		 .getUserData({}, function(err, userData) {
 			if(err) console.error(err);
 			$scope.$apply(function() {
-				$scope.userName = userData.fullname;				
+				$scope.userDataCollection.push(userData);				
 			});
 		 });
 	}
