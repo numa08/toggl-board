37b2186460c43927fa9cf2fd4206c0f7426596ff
numa08 <n511287@gmail.com>
2015-11-26 13:58:57 +0900

    Show current time entry

--- browser/js/toggl-module.js
+++ browser/js/toggl-module.js
@@ -8,8 +8,14 @@ togglBoard.controller('TogglController', ['$scope', function($scope) {
 	
 	$scope.addUser = function() {
 		 new TogglClient({apiToken: $scope.apiToken})
-		 .getUserData({}, function(err, userData) {
+		 .getUserData({with_related_data: true}, function(err, userData) {
 			if(err) console.error(err);
+			var lastTimeEntry = userData.time_entries[userData.time_entries.length - 1];
+			var currentTimeEntry = null;
+			if(lastTimeEntry.duration < 0) {
+				currentTimeEntry = lastTimeEntry;
+			}
+			userData.current_time_entry = currentTimeEntry;			
 			$scope.$apply(function() {
 				$scope.userDataCollection.push(userData);				
 			});
--- main/main.js
+++ main/main.js
@@ -13,8 +13,7 @@ app.on('window-all-closed', function() {
 
 app.on('ready', function() {
 	mainWindow = new BrowserWindow({width: 800, height: 600});
-	mainWindow.loadUrl('file://' + __dirname + '/../browser/index.html');
-	mainWindow.webContents.openDevTools();
+	mainWindow.loadURL('file://' + __dirname + '/../browser/index.html');
 	mainWindow.on('closed', function() {
 		mainWindow = null;
 	});
