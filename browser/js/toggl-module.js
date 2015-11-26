7d4820a2ce1d98ec5a34b4783188ed34def00d05
numa08 <n511287@gmail.com>
2015-11-26 21:17:22 +0900

    load and save user api token to localstorage

--- browser/js/toggl-module.js
+++ browser/js/toggl-module.js
@@ -9,8 +9,34 @@ togglBoard.controller('TogglController', ['$scope', function($scope) {
 	$scope.addUser = function() {
 		 new TogglClient({apiToken: $scope.apiToken})
 		 .getUserData({with_related_data: true}, function(err, userData) {
-			if(err) console.error(err);
-			var lastTimeEntry = userData.time_entries[userData.time_entries.length - 1];
+			if(err){
+				console.error(err);
+				return;	
+			}
+			localStorage.setItem(userData.fullname, userData.api_token);
+			
+			appendUserDataCollection(userData);			
+		 });
+	};
+	
+	(function(){
+		for(var i = 0; i < localStorage.length; i++) {
+			var key = localStorage.key(i);
+			var apiToken = localStorage.getItem(key);
+			new TogglClient({apiToken: apiToken})
+			.getUserData({with_related_data: true}, function(err, userData) {
+				if(err) {
+					console.error(err);
+					return;
+				}
+
+				appendUserDataCollection(userData);				
+			});
+		}
+	})();
+
+	var appendUserDataCollection = function(userData) {
+					var lastTimeEntry = userData.time_entries[userData.time_entries.length - 1];
 			var currentTimeEntry = null;
 			if(lastTimeEntry.duration < 0) {
 				currentTimeEntry = lastTimeEntry;
@@ -20,6 +46,5 @@ togglBoard.controller('TogglController', ['$scope', function($scope) {
 			$scope.$apply(function() {
 				$scope.userDataCollection.push(userData);				
 			});
-		 });
-	}
+	};
 }]);
