7ec419dac7d9bd73cf699d03ad0098289b80a8d0
numa08 <n511287@gmail.com>
2015-12-01 23:40:58 +0900

    disable debug console

--- main/main.js
+++ main/main.js
@@ -14,7 +14,7 @@ app.on('window-all-closed', function() {
 app.on('ready', function() {
 	mainWindow = new BrowserWindow({width: 1000, height: 600});
 	mainWindow.loadURL('file://' + __dirname + '/../browser/index.html');
-	mainWindow.webContents.openDevTools();
+	// mainWindow.webContents.openDevTools();
 	mainWindow.on('closed', function() {
 		mainWindow = null;
 	});
