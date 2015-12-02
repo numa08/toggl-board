3d7b86aa31421183d121e415de50cb11e2ed611f
mironal <miro.gunp@gmail.com>
2015-12-02 21:58:26 +0900

    Screenshot を配信して各自のブラウザから画面をチェックできるようにした
    - ブラウザから `http://<hostname>/ss` を見ると画面の Screenshot が見れる
    - 全てのアカウントで API 叩き終わった後に SS を撮りたかったので async 使って制御するようにした

--- browser/js/toggl-module.js
+++ browser/js/toggl-module.js
@@ -1,18 +1,20 @@
 'use strict';
 const TogglClient = require('toggl-api');
 const moment = require('moment');
+const remote = require('electron').remote;
+const async = require('async');
 
 require('moment-duration-format');
 
 var togglBoard = angular.module('TogglBoard', ['ngDialog']);
 togglBoard.controller('TogglController', function($scope, $interval, ngDialog) {
-	
+
 	$scope.userDataContainer = {
 		users: []
 	};
 
 	$scope.openAddUserPrompt = function() {
-        ngDialog.openConfirm({ 
+        ngDialog.openConfirm({
         	template: 'template-add-user',
         	closeByDocument: true,
         	closeByEscape: true
@@ -26,54 +28,83 @@ togglBoard.controller('TogglController', function($scope, $interval, ngDialog) {
 		 .getUserData({with_related_data: true}, function(err, userData) {
 			if(err){
 				console.error(err);
-				return;	
+				return;
 			}
 			localStorage.setItem(userData.fullname, userData.api_token);
-			
-			appendUserDataContainer(userData);			
+
+			appendUserDataContainer(userData);
 		 });
 	};
-	
-	(function(){
-		for(var i = 0; i < localStorage.length; i++) {
-			var key = localStorage.key(i);
-			var apiToken = localStorage.getItem(key);
-			new TogglClient({apiToken: apiToken})
-			.getUserData({with_related_data: true}, function(err, userData) {
-				if(err) {
-					console.error(err);
-					return;
-				}
 
-				appendUserDataContainer(userData);
-				$interval(function() {
-					for(var i = 0; i < localStorage.length; i++) {
-						var key = localStorage.key(i);
-						var apiToken = localStorage.getItem(key);
-						new TogglClient({apiToken: apiToken})
-						.getUserData({with_related_data: true}, function(err, userData) {
-							if(err) {
-								console.error(err);
-								return;
-							}
-							updateUserDataContainer(userData);
-						})
-					}
-				}, 10 * 1000);
-			
-				$interval(function() {
-					$scope.userDataContainer
-					.users
-					.filter(function(u) {return u.doing_now})
-					.forEach(function(u) {
-						var range = new Date().getTime() - Date.parse(u.current_time_entry.start);
-						u.current_time_entry.duration_output =  moment.duration(range, "ms").format("h [hrs], m [min], s [sec]");
-					});
-				}, 1000);
+  (function(){
 
-			});
-		}
-	})();
+    var saveScreenShot = function() {
+      // Screenshot を取って保存する
+      remote.getCurrentWindow().capturePage(function(img) {
+        if (img.isEmpty()) {
+          return;
+        }
+        const fs = remote.require("fs");
+        fs.writeFile("/tmp/toggle-board-ss.png", img.toPng(), function(err) {
+          if (err) {
+            console.error(err);
+          }
+        });
+      });
+    }
+
+    var apiTokens = function() {
+      var tokens = [];
+      for (var i=0; i < localStorage.length; ++i) {
+        const key = localStorage.key(i);
+        tokens.push(localStorage.getItem(key));
+      }
+      return tokens;
+    }
+
+    async.eachSeries(apiTokens(), function(apiToken, callback) {
+      new TogglClient({apiToken: apiToken})
+      .getUserData({with_related_data: true}, function(err, userData) {
+        appendUserDataContainer(userData);
+        callback(err, userData);
+      });
+    }, function(err) {
+      if (err) {
+        console.error(err);
+      }
+      saveScreenShot();
+    });
+
+    $interval(function() {
+      async.eachSeries(apiTokens(), function(apiToken, callback) {
+        new TogglClient({apiToken: apiToken})
+        .getUserData({with_related_data: true}, function(err, userData) {
+          if(err) {
+            callback(err);
+            return;
+          }
+          updateUserDataContainer(userData);
+          callback(null, userData);
+        })
+      }, function(err){
+        if(err) {
+          console.error(err);
+        }
+        saveScreenShot();
+      });
+
+    }, 10 * 1000);
+
+    $interval(function() {
+      $scope.userDataContainer
+      .users
+      .filter(function(u) {return u.doing_now})
+      .forEach(function(u) {
+        var range = new Date().getTime() - Date.parse(u.current_time_entry.start);
+        u.current_time_entry.duration_output =  moment.duration(range, "ms").format("h [hrs], m [min], s [sec]");
+      });
+    }, 1000);
+  })();
 
 	var appendUserDataContainer = function(userData) {
 			var lastTimeEntry = userData.time_entries[userData.time_entries.length - 1];
@@ -84,12 +115,12 @@ togglBoard.controller('TogglController', function($scope, $interval, ngDialog) {
 				currentTimeEntry.duration_output = moment.duration(range, "ms").format("h [hrs], m [min], s [sec]");
 			}
 			userData.current_time_entry = currentTimeEntry;
-			userData.doing_now = currentTimeEntry != null;	
+			userData.doing_now = currentTimeEntry != null;
 			$scope.$apply(function() {
 				$scope.userDataContainer.users.push(userData);
 			});
 	};
-	
+
 	var updateUserDataContainer = function(userData) {
 			var index = $scope.userDataContainer.users.findIndex(function(u) {
 				return u.fullname === userData.fullname;
@@ -102,11 +133,12 @@ togglBoard.controller('TogglController', function($scope, $interval, ngDialog) {
 			if(lastTimeEntry.duration < 0) {
 				currentTimeEntry = lastTimeEntry;
 				var range = new Date().getTime() - Date.parse(currentTimeEntry.start);
-				currentTimeEntry.duration_output = moment.duration(range, "ms").format("h [hrs], m [min], s [sec]");
-			}
-			userData.current_time_entry = currentTimeEntry;
-			userData.doing_now = currentTimeEntry != null;	
-			$scope.userDataContainer.users[index] = userData;
-	};
+        currentTimeEntry.duration_output = moment.duration(range, "ms").format("h [hrs], m [min], s [sec]");
+      }
+      userData.current_time_entry = currentTimeEntry;
+      userData.doing_now = currentTimeEntry != null;
+      $scope.userDataContainer.users[index] = userData;
 
+  };
 });
+
--- main/main.js
+++ main/main.js
@@ -1,52 +1,80 @@
 var app = require('app');
 var Menu = require('menu');
 var BrowserWindow = require('browser-window');
+var express = require('express');
 
 require('crash-reporter').start();
 
 var mainWindow = null;
 
 app.on('window-all-closed', function() {
-	if(process.platrom != 'darwin') {
-		app.quit();
-	}
+  if(process.platrom != 'darwin') {
+    app.quit();
+  }
 });
 
 app.on('ready', function() {
-	mainWindow = new BrowserWindow({width: 1000, height: 600});
-	mainWindow.loadURL('file://' + __dirname + '/../browser/index.html');
-	// mainWindow.webContents.openDevTools();
-	mainWindow.on('closed', function() {
-		mainWindow = null;
-	});
-	
-	var template = [
-  	{
-    label: 'Edit',
-    submenu: [
-      {
-        label: 'Cut',
-        accelerator: 'Command+X',
-        selector: 'cut:'
-      },
-      {
-        label: 'Copy',
-        accelerator: 'Command+C',
-        selector: 'copy:'
-      },
-      {
-        label: 'Paste',
-        accelerator: 'Command+V',
-        selector: 'paste:'
-      },
-      {
-        label: 'Select All',
-        accelerator: 'Command+A',
-        selector: 'selectAll:'
-      },
-    ]
-  	}];
+  mainWindow = new BrowserWindow({width: 1000, height: 600});
+  mainWindow.loadURL('file://' + __dirname + '/../browser/index.html');
+  // mainWindow.webContents.openDevTools();
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
 
-  	var menu = Menu.buildFromTemplate(template);
-  	Menu.setApplicationMenu(menu);
+  var template = [
+    {
+      label: 'Edit',
+      submenu: [
+        {
+          label: 'Cut',
+          accelerator: 'Command+X',
+          selector: 'cut:'
+        },
+        {
+          label: 'Copy',
+          accelerator: 'Command+C',
+          selector: 'copy:'
+        },
+        {
+          label: 'Paste',
+          accelerator: 'Command+V',
+          selector: 'paste:'
+        },
+        {
+          label: 'Select All',
+          accelerator: 'Command+A',
+          selector: 'selectAll:'
+        },
+      ]
+    }];
+
+    var menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+
+    var server = express();
+
+    // Screenshot を配信
+    server.get("/ss", function(req, res) {
+
+      var options = {
+        dotfiles: 'deny',
+        headers: {
+          'x-timestamp': Date.now(),
+          'x-sent': true
+        }
+      };
+
+      const filename = "/tmp/toggle-board-ss.png";
+      res.sendFile(filename, function(err) {
+        if (err) {
+          console.error(err);
+          res.status(err.status).end();
+        }
+      });
+    });
+
+    const port = process.env.PORT || 9000;
+    server.listen(port);
+    console.log("Start server: PORT = " + port);
 });
+
