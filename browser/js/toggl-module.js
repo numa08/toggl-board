d98dbf9e55932d9a1873215364d1739d77f83395
numa08 <n511287@gmail.com>
2015-12-03 22:42:47 +0900

    use jscs

--- browser/js/toggl-module.js
+++ browser/js/toggl-module.js
@@ -9,190 +9,190 @@ require('moment-duration-format');
 var togglBoard = angular.module('TogglBoard', ['ngDialog']);
 togglBoard.controller('TogglController', function($scope, $interval, ngDialog) {
 
-	$scope.userDataContainer = {
-		users: []
-	};
+    $scope.userDataContainer = {
+        users: []
+    };
 
-  $scope.bulkStopAll = function() {
+    $scope.bulkStopAll = function() {
 
-    async.eachSeries($scope.userDataContainer.users, function(user, callback) {
+        async.eachSeries($scope.userDataContainer.users, function(user, callback) {
 
-      if (user.doing_now === false) {
-        return callback(null, user);
-      }
+            if (user.doing_now === false) {
+                return callback(null, user);
+            }
 
-      var id = user.current_time_entry.id;
-      var apiToken = user.api_token;
-      var client = new TogglClient({apiToken: apiToken});
-      client.stopTimeEntry(id, function(err, timeEntry) {
-        if (err) {
-          return callback(err, null);
-        }
+            var id = user.current_time_entry.id;
+            var apiToken = user.api_token;
+            var client = new TogglClient({apiToken: apiToken});
+            client.stopTimeEntry(id, function(err) {
+                if (err) {
+                    return callback(err, null);
+                }
 
-        client.getUserData({with_related_data: true}, function(err, userData) {
-          updateUserDataContainer(userData);
-          callback(err, user);
+                client.getUserData({with_related_data: true}, function(err, userData) {
+                    updateUserDataContainer(userData);
+                    callback(err, user);
+                });
+            });
+        }, function(err) {
+            if (err) {
+                console.error(err);
+            }
         });
-      });
-    }, function(err) {
-      if (err) {
-        console.error(err);
-      }
-    });
-  }
+    };
 
-  $scope.bulkStartAll = function() {
-    var taskname = $scope.taskName;
-    if (taskname && taskname.length > 0 ) {
+    $scope.bulkStartAll = function() {
+        var taskname = $scope.taskName;
+        if (taskname && taskname.length > 0) {
 
-      async.eachSeries($scope.userDataContainer.users, function(user, callback) {
+            async.eachSeries($scope.userDataContainer.users, function(user, callback) {
 
-        var apiToken = user.api_token;
-        var client = new TogglClient({apiToken: apiToken});
-        client.startTimeEntry({"description": taskname}, function(err, timeEntry) {
-          if (err) {
-            return callback(err, null);
-          }
-          client.getUserData({with_related_data: true}, function(err, userData) {
-            updateUserDataContainer(userData);
-            callback(err, user);
-          });
-        });
+                var apiToken = user.api_token;
+                var client = new TogglClient({apiToken: apiToken});
+                client.startTimeEntry({'description': taskname}, function(err) {
+                    if (err) {
+                        return callback(err, null);
+                    }
+                    client.getUserData({with_related_data: true}, function(err, userData) {
+                        updateUserDataContainer(userData);
+                        callback(err, user);
+                    });
+                });
 
-      }, function(err) {
-        if (err) {
-          console.error(err);
+            }, function(err) {
+                if (err) {
+                    console.error(err);
+                }
+            });
         }
-      });
-    }
-  }
+    };
 
-	$scope.openAddUserPrompt = function() {
+    $scope.openAddUserPrompt = function() {
         ngDialog.openConfirm({
-        	template: 'template-add-user',
-        	closeByDocument: true,
-        	closeByEscape: true
-        }).then(function (apiToken) {
-        	$scope.addUser(apiToken);
+            template: 'template-add-user',
+            closeByDocument: true,
+            closeByEscape: true
+        }).then(function(apiToken) {
+            $scope.addUser(apiToken);
         });
-	}
+    };
 
-	$scope.addUser = function(apiToken) {
-		 new TogglClient({apiToken: apiToken})
-		 .getUserData({with_related_data: true}, function(err, userData) {
-			if(err){
-				console.error(err);
-				return;
-			}
-			localStorage.setItem(userData.fullname, userData.api_token);
-
-			appendUserDataContainer(userData);
-		 });
-	};
-
-  (function(){
-
-    var saveScreenShot = function() {
-      // Screenshot を取って保存する
-      remote.getCurrentWindow().capturePage(function(img) {
-        if (img.isEmpty()) {
-          return;
-        }
-        const fs = remote.require("fs");
-        fs.writeFile("/tmp/toggle-board-ss.png", img.toPng(), function(err) {
-          if (err) {
-            console.error(err);
-          }
-        });
-      });
-    }
-
-    var apiTokens = function() {
-      var tokens = [];
-      for (var i=0; i < localStorage.length; ++i) {
-        const key = localStorage.key(i);
-        tokens.push(localStorage.getItem(key));
-      }
-      return tokens;
-    }
-
-    async.eachSeries(apiTokens(), function(apiToken, callback) {
-      new TogglClient({apiToken: apiToken})
-      .getUserData({with_related_data: true}, function(err, userData) {
-        appendUserDataContainer(userData);
-        callback(err, userData);
-      });
-    }, function(err) {
-      if (err) {
-        console.error(err);
-      }
-      saveScreenShot();
-    });
-
-    $interval(function() {
-      async.eachSeries(apiTokens(), function(apiToken, callback) {
+    $scope.addUser = function(apiToken) {
         new TogglClient({apiToken: apiToken})
-        .getUserData({with_related_data: true}, function(err, userData) {
-          if(err) {
-            callback(err);
-            return;
+      .getUserData({with_related_data: true}, function(err, userData) {
+          if (err) {
+              console.error(err);
+              return;
           }
-          updateUserDataContainer(userData);
-          callback(null, userData);
-        })
-      }, function(err){
-        if(err) {
-          console.error(err);
-        }
-        saveScreenShot();
+          localStorage.setItem(userData.fullname, userData.api_token);
+
+          appendUserDataContainer(userData);
       });
+    };
 
-    }, 10 * 1000);
+    (function() {
 
-    $interval(function() {
-      $scope.userDataContainer
-      .users
-      .filter(function(u) {return u.doing_now})
+        var saveScreenShot = function() {
+            // Screenshot を取って保存する
+            remote.getCurrentWindow().capturePage(function(img) {
+                if (img.isEmpty()) {
+                    return;
+                }
+                const fs = remote.require('fs');
+                fs.writeFile('/tmp/toggle-board-ss.png', img.toPng(), function(err) {
+                    if (err) {
+                        console.error(err);
+                    }
+                });
+            });
+        };
+
+        var apiTokens = function() {
+            var tokens = [];
+            for (var i = 0; i < localStorage.length; ++i) {
+                const key = localStorage.key(i);
+                tokens.push(localStorage.getItem(key));
+            }
+            return tokens;
+        };
+
+        async.eachSeries(apiTokens(), function(apiToken, callback) {
+            new TogglClient({apiToken: apiToken})
+      .getUserData({with_related_data: true}, function(err, userData) {
+          appendUserDataContainer(userData);
+          callback(err, userData);
+      });
+        }, function(err) {
+            if (err) {
+                console.error(err);
+            }
+            saveScreenShot();
+        });
+
+        $interval(function() {
+            async.eachSeries(apiTokens(), function(apiToken, callback) {
+                new TogglClient({apiToken: apiToken})
+        .getUserData({with_related_data: true}, function(err, userData) {
+            if (err) {
+                callback(err);
+                return;
+            }
+            updateUserDataContainer(userData);
+            callback(null, userData);
+        });
+            }, function(err) {
+                if (err) {
+                    console.error(err);
+                }
+                saveScreenShot();
+            });
+
+        }, 10 * 1000);
+
+        $interval(function() {
+            $scope.userDataContainer
+            .users
+            .filter(function(u) {return u.doing_now;})
       .forEach(function(u) {
-        var range = new Date().getTime() - Date.parse(u.current_time_entry.start);
-        u.current_time_entry.duration_output =  moment.duration(range, "ms").format("h [hrs], m [min], s [sec]");
+          var range = new Date().getTime() - Date.parse(u.current_time_entry.start);
+          u.current_time_entry.duration_output =  moment.duration(range, 'ms').format('h [hrs], m [min], s [sec]');
       });
-    }, 1000);
-  })();
+        }, 1000);
+    })();
 
-	var appendUserDataContainer = function(userData) {
-			var lastTimeEntry = userData.time_entries[userData.time_entries.length - 1];
-			var currentTimeEntry = null;
-			if(lastTimeEntry.duration < 0) {
-				currentTimeEntry = lastTimeEntry;
-				var range = new Date().getTime() - Date.parse(currentTimeEntry.start);
-				currentTimeEntry.duration_output = moment.duration(range, "ms").format("h [hrs], m [min], s [sec]");
-			}
-			userData.current_time_entry = currentTimeEntry;
-			userData.doing_now = currentTimeEntry != null;
-			$scope.$apply(function() {
-				$scope.userDataContainer.users.push(userData);
-			});
-	};
+    var appendUserDataContainer = function(userData) {
+        var lastTimeEntry = userData.time_entries[userData.time_entries.length - 1];
+        var currentTimeEntry = null;
+        if (lastTimeEntry.duration < 0) {
+            currentTimeEntry = lastTimeEntry;
+            var range = new Date().getTime() - Date.parse(currentTimeEntry.start);
+            currentTimeEntry.duration_output = moment.duration(range, 'ms').format('h [hrs], m [min], s [sec]');
+        }
+        userData.current_time_entry = currentTimeEntry;
+        userData.doing_now = currentTimeEntry != null;
+        $scope.$apply(function() {
+            $scope.userDataContainer.users.push(userData);
+        });
+    };
 
-	var updateUserDataContainer = function(userData) {
-			var index = $scope.userDataContainer.users.findIndex(function(u) {
-				return u.fullname === userData.fullname;
-			});
-			if(index < 0) {
-				return;
-			}
-			var lastTimeEntry = userData.time_entries[userData.time_entries.length - 1];
-			var currentTimeEntry = null;
-			if(lastTimeEntry.duration < 0) {
-				currentTimeEntry = lastTimeEntry;
-				var range = new Date().getTime() - Date.parse(currentTimeEntry.start);
-        currentTimeEntry.duration_output = moment.duration(range, "ms").format("h [hrs], m [min], s [sec]");
-      }
-      userData.current_time_entry = currentTimeEntry;
-      userData.doing_now = currentTimeEntry != null;
-      $scope.userDataContainer.users[index] = userData;
+    var updateUserDataContainer = function(userData) {
+        var index = $scope.userDataContainer.users.findIndex(function(u) {
+            return u.fullname === userData.fullname;
+        });
+        if (index < 0) {
+            return;
+        }
+        var lastTimeEntry = userData.time_entries[userData.time_entries.length - 1];
+        var currentTimeEntry = null;
+        if (lastTimeEntry.duration < 0) {
+            currentTimeEntry = lastTimeEntry;
+            var range = new Date().getTime() - Date.parse(currentTimeEntry.start);
+            currentTimeEntry.duration_output = moment.duration(range, 'ms').format('h [hrs], m [min], s [sec]');
+        }
+        userData.current_time_entry = currentTimeEntry;
+        userData.doing_now = currentTimeEntry != null;
+        $scope.userDataContainer.users[index] = userData;
 
-  };
+    };
 });
 
--- main/main.js
+++ main/main.js
@@ -8,43 +8,43 @@ require('crash-reporter').start();
 var mainWindow = null;
 
 app.on('window-all-closed', function() {
-  if(process.platrom != 'darwin') {
-    app.quit();
-  }
+    if (process.platrom != 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('ready', function() {
-  mainWindow = new BrowserWindow({width: 1000, height: 600});
-  mainWindow.loadURL('file://' + __dirname + '/../browser/index.html');
-  // mainWindow.webContents.openDevTools();
-  mainWindow.on('closed', function() {
-    mainWindow = null;
-  });
+    mainWindow = new BrowserWindow({width: 1000, height: 600});
+    mainWindow.loadURL('file://' + __dirname + '/../browser/index.html');
+    // mainWindow.webContents.openDevTools();
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    });
 
-  var template = [
+    var template = [
     {
-      label: 'Edit',
-      submenu: [
+        label: 'Edit',
+        submenu: [
         {
-          label: 'Cut',
-          accelerator: 'Command+X',
-          selector: 'cut:'
+            label: 'Cut',
+            accelerator: 'Command+X',
+            selector: 'cut:'
         },
         {
-          label: 'Copy',
-          accelerator: 'Command+C',
-          selector: 'copy:'
+            label: 'Copy',
+            accelerator: 'Command+C',
+            selector: 'copy:'
         },
         {
-          label: 'Paste',
-          accelerator: 'Command+V',
-          selector: 'paste:'
+            label: 'Paste',
+            accelerator: 'Command+V',
+            selector: 'paste:'
         },
         {
-          label: 'Select All',
-          accelerator: 'Command+A',
-          selector: 'selectAll:'
-        },
+            label: 'Select All',
+            accelerator: 'Command+A',
+            selector: 'selectAll:'
+        }
       ]
     }];
 
@@ -54,27 +54,27 @@ app.on('ready', function() {
     var server = express();
 
     // Screenshot を配信
-    server.get("/ss", function(req, res) {
+    server.get('/ss', function(req, res) {
 
-      var options = {
-        dotfiles: 'deny',
-        headers: {
-          'x-timestamp': Date.now(),
-          'x-sent': true
-        }
-      };
+        var options = {
+            dotfiles: 'deny',
+            headers: {
+                'x-timestamp': Date.now(),
+                'x-sent': true
+            }
+        };
 
-      const filename = "/tmp/toggle-board-ss.png";
-      res.sendFile(filename, function(err) {
-        if (err) {
-          console.error(err);
-          res.status(err.status).end();
-        }
-      });
+        const filename = '/tmp/toggle-board-ss.png';
+        res.sendFile(filename, function(err) {
+            if (err) {
+                console.error(err);
+                res.status(err.status).end();
+            }
+        });
     });
 
     const port = process.env.PORT || 9000;
     server.listen(port);
-    console.log("Start server: PORT = " + port);
+    console.log('Start server: PORT = ' + port);
 });
 
